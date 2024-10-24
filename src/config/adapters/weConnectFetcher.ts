432fbef5a2b5619ce457af6e855469f671cda349
Nacho Trevisan <nachotizii988@gmail.com>
2024-10-24 10:36:02 -0300

    Se agrego las funcionalidades de  modificacion y eliminacion de post

--- src/config/adapters/weConnectFetcher.ts
+++ src/config/adapters/weConnectFetcher.ts
@@ -3,7 +3,7 @@ import { AxiosAdapter } from "./http/axios.adapter";
 
 
 export const WeConnectFetcher = new AxiosAdapter({
-    baseUrl: 'http://192.168.0.104:4000/api/', params: {
+    baseUrl: 'https://weconnectbackend.onrender.com/api/', params: {
 
         //RECORDAR QUE AL CAMBIAR EL BASEURL LLEVA HTTPS, EN LOCALHOST NO.
     }
